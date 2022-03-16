3168295e6a8c83d49f74d15c23ce3c86aac8569c
gregs-hub <36857267+gregs-hub@users.noreply.github.com>
2022-03-16 10:07:48 +0100

    Update and rename 01_premier_contrat.sol to 01_first_contract.sol

--- 01_first_contract.sol
+++ 01_first_contract.sol
@@ -4,7 +4,7 @@ pragma solidity >= 0.8.7 < 0.9;
 contract test {
 
     // Integers
-    uint nombre; //2**8 = 256 uint == uint256. Valeur par défaut d'un uint est zero (pas une valeur vide)
+    uint nombre; //2**8 = 256 uint == uint256. uint default value is zero
 
     function getNombre() public view returns(uint) {
         return nombre;
