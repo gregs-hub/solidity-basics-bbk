2b432d005dd14a1c66e6fb532af5e3f6fda82879
gregs-hub <36857267+gregs-hub@users.noreply.github.com>
2022-03-28 17:49:06 +0200

    Add revert-require-assert

---
+++ contracts/10_revert-require-assert.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.7 < 0.9;
+
+contract Exercise {
+
+    // revert (with more complex logic) and require : will allow you to return a value, will refund any remaining gas to the caller
+    // assert: to prevent anything really bad from happening
+    
+    uint number;
+
+    function setNumber(uint _number) public {
+        require(_number != 10, "number can't be equal to 10"); // require(condition, otherwise)
+        number = _number;
+    }
+
+    function getNumber() public view returns(uint) {
+        return number;
+    }
+
+}
